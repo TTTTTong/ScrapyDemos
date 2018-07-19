092d26442a4bf75d2691e5ceee4239335ff73e2d
tong <t15529208705@gmail.com>
2018-07-19 17:17:48 +0800

    many

--- exchange/exchange/mysqlpipeline/pipelines.py
+++ exchange/exchange/mysqlpipeline/pipelines.py
@@ -1,4 +1,5 @@
 from .sql import sql
+import time
 from exchange.items import RegalHolderItem, UnusualItem, TokenInfoItem, IpItem,\
     CurrencyHolderInfoItemEs, CurrencyHolderInfoItemTv
 
@@ -7,11 +8,13 @@ class ExchangePipeline:
     def process_item(self, item, spider):
 
         if isinstance(item, RegalHolderItem):
-            sql.insert_regal_holder(item['currency'], item['rank'], item['address'], item['quantity'], item['per'])
+            sql.insert_regal_holder(item['currency'], item['rank'], item['address'], item['quantity'], item['per'],
+                                    item['exchange'])
 
         if isinstance(item, UnusualItem):
             sql.insert_unusual(item['currency'], item['txid'], item['quantity'], item['time'],
-                             item['is_regal'], item['from_addr'], item['to_addr'])
+                               item['is_regal'], item['from_addr'], item['to_addr'], item['from_exchange'],
+                               item['to_exchange'])
 
         if isinstance(item, CurrencyHolderInfoItemEs):
             sql.insert_currency_info_fromes(item['currency'], item['holders'], item['day_price'], item['trans'])
@@ -19,6 +22,7 @@ class ExchangePipeline:
         if isinstance(item, CurrencyHolderInfoItemTv):
             sql.insert_currency_info_fromtv(item['currency'], item['day_price'], item['concentration_10'],
                                             item['concentration_50'], item['concentration_100'])
+
         if isinstance(item, TokenInfoItem):
             sql.indert_tokeninfo(item['currency'], item['contract'])
 
@@ -26,6 +30,8 @@ class ExchangePipeline:
             sql.insert_ip(item['ip'])
 
     def close_spider(self, spider):
-        print('eeeeeeeend')
+        with open('/srv/node-app/crawl/exchange/close_spider.log', 'a+') as f:
+            f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ':  ' + spider.name + ' finish\n')
 
-
+    # def start_spider(self, spider):
+    #     print('ssssss')
